3fb1799d4ea62643a5914dd66ade632239d7bfec
Puh00 <alex.phu@live.com>
2020-07-09 15:35:59 +0200

    UserBookStatus class and BookStatus (enum)

---
+++ Temporary_Backend/src/BookStatus.java
@@ -0,0 +1,4 @@
+
+public enum BookStatus {
+    CURRENTLY_READING, WANT_TO_READ, HAS_READ
+}
--- Temporary_Backend/src/UserBookStatus.java
+++ Temporary_Backend/src/UserBookStatus.java
@@ -2,4 +2,29 @@
 
 public class UserBookStatus {
 
+    private final Book book;
+    private final User user;
+    private BookStatus bookStatus;
+
+    public UserBookStatus(Book book, User user, BookStatus bookStatus) {
+        this.book = book;
+        this.user = user;
+        this.bookStatus = bookStatus;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public BookStatus getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(BookStatus bookStatus) {
+        this.bookStatus = bookStatus;
+    }
 }
