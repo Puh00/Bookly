d83545aaae9d4383fc2ef6f39eb58a9c3bb2e76c
Puh00 <alex.phu@live.com>
2020-07-09 15:15:15 +0200

    LoginTool class

---
+++ Temporary_Backend/src/LoginTool.java
@@ -0,0 +1,22 @@
+
+
+public class LoginTool {
+
+    private User user;
+
+    public LoginTool(User user) {
+        this.user = user;
+    }
+
+    public boolean login(String username, String password) {
+        return confirmUsername(username) && confirmPassword(password);
+    }
+
+    private boolean confirmUsername(String username) {
+        return user.getUserName().equals(username);
+    }
+
+    private boolean confirmPassword(String password) {
+        return user.getPassword().equals(password);
+    }
+}
