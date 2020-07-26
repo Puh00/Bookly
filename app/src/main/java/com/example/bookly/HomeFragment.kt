960da8e81fcc020cc07d3a49fcc08f4084051555
Puh00 <alex.phu@live.com>
2020-07-26 17:05:10 +0200

    Removed default appbar, hamburger button still works

--- app/src/main/java/com/example/bookly/HomeFragment.kt
+++ app/src/main/java/com/example/bookly/HomeFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.bookly.databinding.FragmentHomeBinding
-import com.example.bookly.databinding.FragmentMyBooksBinding
 
 class HomeFragment: Fragment() {
 
 
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,6 +31,20 @@ class HomeFragment: Fragment() {
             view.findNavController().navigate(R.id.action_homeFragment_to_myReviewsFragment)
         }
 
+        //binding.root.drawerLayout.openDrawer(GravityCompat.START)
+        val tempButton: Button = binding.tempoBtn
+        tempButton.setOnClickListener {         (activity as MainActivity?)?.yolo()
+        }
+
+
+
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
 }
--- app/src/main/java/com/example/bookly/MainActivity.kt
+++ app/src/main/java/com/example/bookly/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bookly
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -13,7 +14,7 @@ import com.example.bookly.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
+    public lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         val navController: NavController = this.findNavController(R.id.myNavHostFragment)
         drawerLayout = binding.drawerLayout
 
+/*
         // Creates a set of top level destinations and saves it in the appBarConfiguration
         val topLevelDestinations = setOf(R.id.homeFragment, R.id.myBooksFragment, R.id.myReviewsFragment)
         appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations).setDrawerLayout(drawerLayout).build()
@@ -30,15 +32,19 @@ class MainActivity : AppCompatActivity() {
         // Setup side panel
         NavigationUI.setupWithNavController(binding.navView, navController)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+*/
 
 
+    }
+
+    public fun yolo(){
+        drawerLayout.openDrawer(GravityCompat.START)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
-
 
 
 }
